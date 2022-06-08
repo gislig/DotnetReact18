fec9724e2a0a203ff49283dc20c59ed4c3a0b655
Gisli Gudmundsson <gisli@iteggs.com>
2022-06-08 22:36:42 +0000

    fix in react18 project and updated home.js

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <p>Welcome to your new single-page application using React 18, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
@@ -20,7 +20,12 @@ export class Home extends Component {
           <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <br />
+        <p><a href="https://github.com/gislig/DotnetReact18">Updated by Gísli Guðmundsson</a></p>
+        <p><a href="https://is.linkedin.com/in/gisli-gudmundsson-11a77639>">Linkedin</a></p>
+        <p><a href="https://mvp.microsoft.com/en-us/PublicProfile/5004604?fullName=G%C3%ADsli%20Gu%C3%B0mundsson">Microsoft MVP</a></p>
       </div>
     );
   }
 }
+
